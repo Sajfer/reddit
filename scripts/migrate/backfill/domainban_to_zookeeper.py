17d5f8a4367aa91c10229a8fb9066906113bbe46
Meiso <meiha91@gmail.com>
2014-09-22 09:27:07 +0200

    test

--- scripts/migrate/backfill/domainban_to_zookeeper.py
+++ scripts/migrate/backfill/domainban_to_zookeeper.py
@@ -10,7 +10,7 @@ zkbans = LiveDict(g.zookeeper, "/banned-domains", watch=False)
 hcb = g.hardcache.backend
 for domain in hcb.ids_by_category("domain", limit=5000):
     domain_info = g.hardcache.get("domain-" + domain)
-    zkbans[domain] = domain_info
+    zkbans[domain] = domain_info.
 
 
 
